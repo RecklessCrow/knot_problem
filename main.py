f3a1c01d56ddd7ae1f4b413b077afbe840052cf6
CJ <jmcc2646@gmail.com>
2021-02-15 09:49:40 -0500

    updated how to find start and end

--- main.py
+++ main.py
@@ -23,56 +23,119 @@ def get_matrix():
     return [m for m in matrix if m]
 
 
+def find_start_end(puzzle):
+    result = np.where(puzzle == '-')
+    puzzle_pos = list(zip(result[0], result[1]))
+    start = None
+    end = None
+
+    if 0 in result[1]:
+        positions = np.where(result[1] == 0)[0]
+
+        row = True
+        reverse = False
+
+        start = puzzle_pos[positions[0]]
+
+        if len(positions) > 1:
+            end = puzzle_pos[positions[1]]
+            return start, end, row, reverse
+
+    if len(puzzle[0]) - 1 in result[1]:
+        positions = np.where(result[1] == len(puzzle[0]) - 1)[0]
+
+        if start is None:
+            row = True
+            reverse = True
+            start = puzzle_pos[positions[0]]
+            if len(positions) > 1:
+                end = puzzle_pos[positions[1]]
+                return start, end, row, reverse
+        else:
+            end = puzzle_pos[positions[0]]
+            return start, end, row, reverse
+
+    result = np.where(puzzle == '|')
+    puzzle_pos = list(zip(result[0], result[1]))
+
+    if 0 in result[0]:
+        positions = np.where(result[0] == 0)[0]
+
+        if start is None:
+            row = False
+            reverse = False
+            start = puzzle_pos[positions[0]]
+            if len(positions) > 1:
+                end = puzzle_pos[positions[1]]
+                return start, end, row, reverse
+        else:
+            end = puzzle_pos[positions[0]]
+            return start, end, row, reverse
+
+    if len(puzzle) - 1 in result[0]:
+        positions = np.where(result[0] == len(puzzle) - 1)[0]
+
+        if start is None:
+            row = False
+            reverse = False
+            start = puzzle_pos[positions[0]]
+            if len(positions) > 1:
+                end = puzzle_pos[positions[1]]
+                return start, end, row, reverse
+        else:
+            end = puzzle_pos[positions[0]]
+            return start, end, row, reverse
+
+    return None, None, None, None
+
+
 # useing gauss code
 def puzzle_solver(puzzle):
-    row_idx = int(np.where(puzzle[:, 0] == '-')[0])
-    col_idx = 0
     crosses = ""
 
-    row = True
-    reverse = False
+    start, end, row, reverse = find_start_end(puzzle)
 
-    try:
-        while True:
-            pos = (row_idx, col_idx)
+    row_idx, col_idx = start
+    pos = (row_idx, col_idx)
 
-            char = puzzle[pos]
+    while pos != end:
+        char = puzzle[pos]
 
-            if row:
-                if char == 'H':
-                    crosses += 'H'
-                elif char == 'I':
-                    crosses += 'I'
+        if row:
+            if char == 'H':
+                crosses += 'H'
+            elif char == 'I':
+                crosses += 'I'
 
-                if char == '+':
-                    row = False
-                    if puzzle[row_idx - 1, col_idx] == '|':
-                        reverse = True
-                        row_idx -= 1
-                    else:
-                        reverse = False
-                        row_idx += 1
+            if char == '+':
+                row = False
+                if puzzle[row_idx - 1, col_idx] == '|':
+                    reverse = True
+                    row_idx -= 1
                 else:
-                    col_idx += 1 if not reverse else -1
-
+                    reverse = False
+                    row_idx += 1
             else:
-                if char == 'H':
-                    crosses += 'I'
-                elif char == 'I':
-                    crosses += 'H'
+                col_idx += 1 if not reverse else -1
 
-                if char == '+':
-                    row = True
-                    if puzzle[row_idx, col_idx - 1] == '-':
-                        reverse = True
-                        col_idx -= 1
-                    else:
-                        reverse = False
-                        col_idx += 1
+        else:
+            if char == 'H':
+                crosses += 'I'
+            elif char == 'I':
+                crosses += 'H'
+
+            if char == '+':
+                row = True
+                if puzzle[row_idx, col_idx - 1] == '-':
+                    reverse = True
+                    col_idx -= 1
                 else:
-                    row_idx += 1 if not reverse else -1
-    except IndexError:
-        pass
+                    reverse = False
+                    col_idx += 1
+            else:
+                row_idx += 1 if not reverse else -1
+
+        pos = (row_idx, col_idx)
 
     return crosses
 
